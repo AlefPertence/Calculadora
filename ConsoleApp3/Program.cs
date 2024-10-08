2000f2f377502236199a04f7613679518945dc27
Alef Souza Pertence <alefpertencedev@gmail.com>
2024-10-07 21:37:35 -0300

    email dev

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,89 +1,48 @@
 ﻿using System;
 using System.ComponentModel.Design;
 
-namespace Calculadora
+using System.Net.NetworkInformation;
+
+namespace CalculoDeMedia
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            int num1 = 0;
-            int num2 = 0;
-            int result = 0;
-            char operacao;
-            char resp1 = 'S';
-            char resp2 = 's';
-            char resposta;
 
-            do
+            double nota1 = 0;
+            double nota2 = 0;
+            double nota3 = 0;
+            double nota4 = 0;
+            Console.WriteLine("Digite as notas para saber se está áprovado ou reprovado");
+            Console.WriteLine("se sua nota for igual ou superior a 6 será considerado: APROVADO. Caso seja inferior a 6 será considerado: REPROVADO");
+          
+            Console.Write("digite sua 1° nota: ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua 2° nota: ");
+            nota2 = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua 3° nota: ");
+            nota3 = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua 4° nota: ");
+            nota4 = double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            if (media >= 6 && media <= 10)
             {
-                Console.WriteLine("Qual operação você deseja realizar?");
-                Console.WriteLine("Operação (1): adição +");
-                Console.WriteLine("Operação (2): subtração -");
-                Console.WriteLine("Operação (3): multiplicação *");
-                Console.WriteLine("Operação (4): divisão /");
-
-                operacao = char.Parse(Console.ReadLine());
-                switch (operacao)
-                {
-                    case '1':
-                        Console.WriteLine("Digite o primeiro número");
-                        num1 = int.Parse(Console.ReadLine());
-                        Console.WriteLine("digite o seu segundo número");
-                        num2 = int.Parse(Console.ReadLine());
-
-                        result = num1 + num2;
-
-                        break;
-
-                    case '2':
-                        Console.WriteLine("Digite o primeiro número");
-                        num1 = int.Parse(Console.ReadLine());
-                        Console.WriteLine("digite o seu segundo número");
-                        num2 = int.Parse(Console.ReadLine());
-
-                        result = num1 - num2;
-                        break;
-
-                    case '3':
-                        Console.WriteLine("Digite o primeiro número");
-                        num1 = int.Parse(Console.ReadLine());
-                        Console.WriteLine("digite o seu segundo número");
-                        num2 = int.Parse(Console.ReadLine());
-
-                        result = num1 * num2;
-                        break;
-
-                    case '4':
-                        Console.WriteLine("Digite o primeiro número");
-                        num1 = int.Parse(Console.ReadLine());
-                        Console.WriteLine("digite o seu segundo número");
-                        num2 = int.Parse(Console.ReadLine());
-
-                        result = num1 / num2;
-                        break;
-
-                    default:
-                        Console.WriteLine("Essa opção não existe! ");
-
-                        break;
-
-                }
-
-                Console.WriteLine("O resultado é: {0}", result);
-               
-                Console.WriteLine("Deseja continuar calculando? se SIM digite S,caso contrário aperte qualquer tecla para encerrar.");
-                resposta = Console.ReadKey().KeyChar;
-                resp1 = resposta;
-                resp2 = resposta;
-
-
-
-            } while (resposta == 's' || resposta == 'S');
-
+                Console.WriteLine("Sua média é: {0} {1} ", media,". O aluno(a) está aprovado(a)");
+                
+            }
             
-
-
+            else if(media <= 6 && media >= 0)
+            {
+                Console.WriteLine("Sua média é: {0} {1}", media,". O aluno(a) está reporivado(a)");
+            }
+            
+            else
+            {
+                Console.WriteLine("Média incorreta.");
+            }
         }
     }
 }
